0be48bada5396b22dd4e7d71b5ff3ce1a1142cd4
Admin <19vinhnam98@gmail.com>
2025-02-23 14:26:45 +0700

    contact

--- src/main/java/com/example/demo/controller/home/HomeController.java
+++ src/main/java/com/example/demo/controller/home/HomeController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller.home;
 
+import com.example.demo.model.entity.Contact;
+import com.example.demo.service.ContactService;
 import com.example.demo.service.CustomersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @Autowired
     private CustomersService customersService;
+    private ContactService contactService;
     @GetMapping
     public String index() {
 
@@ -25,4 +29,9 @@ public class HomeController {
         return "master/main_home";
     }
 
+    @PostMapping()
+    public String addContact(@ModelAttribute("contact") Contact contact, Model model) {
+        contactService.save(contact);
+        return "redirect:/";
+    }
 }
