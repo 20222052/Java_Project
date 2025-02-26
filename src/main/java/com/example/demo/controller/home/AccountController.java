f633cc4b1d1041110b1a878269c9d69271e944eb
Admin <19vinhnam98@gmail.com>
2025-02-26 23:00:19 +0700

    login

--- src/main/java/com/example/demo/controller/home/AccountController.java
+++ src/main/java/com/example/demo/controller/home/AccountController.java
@@ -1,4 +1,19 @@
 package com.example.demo.controller.home;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/acc")
 public class AccountController {
+    @GetMapping
+    public String login() {
+        return "account/login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "account/register";
+    }
 }
